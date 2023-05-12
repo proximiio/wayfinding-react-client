fa152c6807819c618aaceb875966e198c46904f0
Matej Drlicka <sprattek@gmail.com>
2023-05-12 10:42:48 +0200

    feat: add environment variables for Proximi.io wayfinding client
    Add `.env` file with environment variables for Proximi.io wayfinding client.
    - `BASE_URL` sets the base URL for the client
    - `PORT` sets the port for the client
    
    Update `index.html` to use `%VITE_APP_TITLE%` as the title.
    
    Add `proximiio-js-library` as a dependency.
    
    Update `vite-env.d.ts` to include the new environment variables.
    
    Update `vite.config.ts` to load the environment variables and set the base URL and port for the client.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+	readonly VITE_APP_TITLE: string;
+	readonly VITE_PROXIMIIO_TOKEN: string;
+	readonly VITE_PROXIMIIO_API: string;
+	readonly VITE_PROXIMIIO_GEO_VERSION: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
