d3551841b5d1dbce9a20eea0e2d55c925d5054fb
Matej Drlicka <sprattek@gmail.com>
2023-05-12 15:42:47 +0200

    fix(.env): remove trailing slash from BASE_URL fix(.env): change placeholder text to 'PASTE' in VITE_PROXIMIIO_TOKEN and VITE_WAYFINDING_DEFAULT_PLACE_ID feat(App.tsx): add useEffect hook to handle URL query parameters for language and kiosk mode feat(App.tsx): add idleTime function to handle timeouts to reset to default view feat(App.tsx): add resetView function to handle reset to default view feat(App.tsx): pass kioskMode and currentLang as props to MapView component feat(MapView.tsx): create MapView component to handle map initialization and authentication feat(MapView.tsx): add useEffect hook to initialize map and authenticate Proximiio.js library feat(MapView.tsx): pass map instance to parent component using onSetMap prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,69 @@
-import Proximiio from 'proximiio-js-library';
+import { useEffect, useState } from 'react';
+import MapView from './components/MapView';
+import { Map } from 'proximiio-js-library/lib/components/map/main';
 
 function App() {
-	const zoom = 19;
-	const pitch = 40;
-	const bearing = 12;
-	const kioskMode = false;
-	const defaultPlaceId = import.meta.env.VITE_WAYFINDING_DEFAULT_PLACE_ID;
-	const defaultLocation: {
-		coordinates: [number, number];
-		level: number;
-	} = {
-		coordinates: [51.48091652702158, 25.336680584406395],
-		level: 0,
-	};
-	const mapPadding = {
-		top: 250,
-		bottom: 250,
-		left: 500,
-		right: 250,
-	};
-	const currentLang = 'en';
+	const [map, setMap] = useState({} as Map);
+	const [kioskMode, setKioskMode] = useState(false);
+	const [currentLang, setCurrentLang] = useState('en');
 
-	// Proximiio.js library authentification is done here
-	Proximiio.Auth.loginWithToken(import.meta.env.VITE_PROXIMIIO_TOKEN).then(
-		() => {
-			// when authenticated create a map instance with provided constuctor options, check out library readme https://github.com/proximiio/proximiio-js-library#map-component
-			new Proximiio.Map({
-				// you can define any of the mapbox options in there, we are retrieving those from state service
-				mapboxOptions: {
-					zoom: zoom,
-					pitch: pitch,
-					bearing: bearing,
-				},
-				defaultPlaceId: defaultPlaceId, // if you have more than 1 place in your account, it's a good idea to define defaultPlaceId for the map, otherwise the first one will be picked up
-				isKiosk: kioskMode, // if enabled starting point for routing will be based on values defined in kioskSettings, if disabled findRoute methods will expect start point to be send.
-				kioskSettings: {
-					coordinates: defaultLocation.coordinates,
-					level: defaultLocation.level,
-				},
-				fitBoundsPadding: mapPadding, // setting the padding option to use for zooming into the bounds when route is drawn,
-				handleUrlParams: true, // enable handling url params, this way you can load map with predefined route generated
-				language: currentLang, // init with predefined language setting
-				// useGpsLocation: true, // if enabled your location will be detected with geolocation API and used as a starting point for routing
-				// geolocationControlOptions: {
-				//   autoTrigger: true, // if enabled map will automatically enable geolocation
-				//   autoLocate: false, // if enabled map will automatically focus on user location
-				//   position: 'bottom-right', //  position on the map to which the control will be added.
-				// },
-				showLevelDirectionIcon: true, // if enabled arrow icon will be shown at the levelchanger indicating direction of level change along the found route
-				initPolygons: true,
-				animatedRoute: true,
-			});
+	// This effect hook handles URL query parameters related to language and kiosk mode
+	useEffect(() => {
+		// Extract URL query parameters
+		const urlParams = new URLSearchParams(window.location.search);
+
+		// Retrieve 'language' parameter from URL or default to 'en' if not found
+		const urlLanguage = urlParams.get('language') ?? 'en';
+
+		// If 'kiosk' parameter exists in URL, set kiosk mode
+		if (urlParams.get('kiosk')) {
+			setKioskMode(true);
 		}
-	);
+
+		// Update language state with 'language' parameter from URL
+		setCurrentLang(urlLanguage);
+	}, [setKioskMode, setCurrentLang]);
+
+	// idleTime function handle timeouts to reset to default view
+	const idleTime = () => {
+		let t: ReturnType<typeof setTimeout>;
+
+		// set timer and call reset view method on timeout
+		const resetTimer = () => {
+			clearTimeout(t);
+			t = setTimeout(resetView, 1 * 60000); // 1 minute
+		};
+
+		window.onload = resetTimer;
+		window.onmousemove = resetTimer;
+		window.onmousedown = resetTimer; // catches touchscreen presses as well
+		window.ontouchstart = resetTimer; // catches touchscreen swipes as well
+		window.ontouchmove = resetTimer; // required by some devices
+		window.onclick = resetTimer; // catches touchpad clicks as well
+		window.onkeydown = resetTimer;
+		window.addEventListener('scroll', resetTimer, true); // improved; see comments*/
+	};
+
+	// handle reset to default view
+	const resetView = () => {
+		if (Object.keys(map).length > 0) {
+			map.refetch();
+		}
+	};
+
+	// if app is initiated in kioskMode run idleTime function
+	if (kioskMode) {
+		idleTime();
+	}
+
 	return (
 		<>
 			<main>
-				<div id='proximiioMap' className='w-screen h-screen'></div>
+				<MapView
+					kioskMode={kioskMode}
+					currentLang={currentLang}
+					onSetMap={setMap}
+				/>
 			</main>
 		</>
 	);
