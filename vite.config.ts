1527d070c366a1b35969d0f811a76d3b107d6ae5
Matej Drlicka <sprattek@gmail.com>
2023-12-21 14:05:00 +0100

    use vite compress
    update nginx.conf to handle gzip"

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -57,6 +58,7 @@ export default defineConfig(({ mode }) => {
 					background_color: '#000000',
 				},
 			}),
+			viteCompression(),
 		],
 		base: env.BASE_URL,
 		preview: {
