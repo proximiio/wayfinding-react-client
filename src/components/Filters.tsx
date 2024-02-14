2b5a32137d0b329d3b117e2f2a310dadf11ea050
Matej Drlicka <sprattek@gmail.com>
2024-02-14 13:12:05 +0100

    chore(Filters.tsx): update className to add max height and overflow properties to improve UI layout and scrolling functionality

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -15,7 +15,7 @@ function Filters() {
 				animate={{ opacity: 1, scale: 1 }}
 				exit={{ opacity: 0, scale: 0 }}
 				transition={{ duration: 0.2 }}
-				className='absolute top-0 left-0 right-0 z-20 p-3 pt-5 bg-white shadow-lg sm:z-10 rounded-b-md sm:shadow-none sm:border-2 sm:rounded-lg sm:bg-white/80 sm:top-5 lg:top-20 sm:left-5 sm:p-5 sm:pt-16 sm:right-auto'
+				className='absolute top-0 left-0 right-0 z-20 p-3 pt-5 bg-white shadow-lg sm:z-10 rounded-b-md sm:shadow-none sm:border-2 sm:rounded-lg sm:bg-white/80 sm:top-5 lg:top-20 sm:left-5 sm:p-5 sm:pt-16 sm:right-auto lg:max-h-[90%] lg:overflow-auto'
 			>
 				{filterCategories.map((category) => (
 					<FilterPicker
